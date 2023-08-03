f8f5f55f81c50886e21da81c96800f8258270eae
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-08-03 15:33:56 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import City from "./routes/city";
+import Forecast from "./routes/forecast";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -10,6 +12,16 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/city/:cityid",
+        element: <City />,
+      },
+      {
+        path: "/forecast/:forecastid",
+        element: <Forecast />,
+      },
+    ],
   },
 ]);
 
